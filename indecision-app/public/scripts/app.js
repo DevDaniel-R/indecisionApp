2c6590f8ea5a81d04c715c2ea8a2a94410f6f2d2
Daniel Rodriguez <drodriguez661@gmail.com>
2019-07-07 09:00:53 -0500

    es6 classes file added to playground folder

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -6,48 +6,90 @@ console.log('App.js is running');
 var app = {
   title: 'Indecision App',
   subTitle: 'my little pony 2',
-  options: ['one', 'Two']
+  options: []
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    'p',
-    null,
-    ' ',
-    app.subTitle,
-    ' '
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length >= 0 ? 'here are your options' : 'no options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'item three'
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderFormApp();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  renderFormApp();
+};
 
 var appRoot = document.getElementById('app');
+
+var renderFormApp = function renderFormApp() {
+
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      'p',
+      null,
+      ' ',
+      app.subTitle,
+      ' '
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length >= 0 ? 'here are your options' : 'no options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: onRemoveAll },
+      'remove all'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'item three'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+renderFormApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,20 +4,48 @@ console.log('App.js is running');
 const app ={
 title: 'Indecision App',
 subTitle: 'my little pony 2',
-options: ['one', 'Two']
+options: []
 };
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderFormApp();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  renderFormApp();
+}
+
+const appRoot = document.getElementById('app');
+
+const renderFormApp = () => {
 
 const template = (
   <div>
     <h1>{app.title}</h1>
     {app.subTitle && <p> {app.subTitle} </p>}
     <p>{app.options.length >= 0 ? 'here are your options' : 'no options'}</p>
+    <p>{app.options.length}</p>
+    <button onClick ={onRemoveAll}>remove all</button>
       <ol>
         <li>item one</li>
         <li>item one</li>
         <li>item three</li>
       </ol>
+      <form onSubmit={onFormSubmit}>
+      <input type="text" name="option"></input>
+      <button>Add Option</button>
+      </form>
   </div>
 );
-
-const appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
+};
+renderFormApp();
